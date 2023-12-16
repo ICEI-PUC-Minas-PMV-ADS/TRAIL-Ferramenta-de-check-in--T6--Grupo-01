52c0b9a1bf3e167ba199599f12363791edd2f737
willianbarbosadeandrade <willianbarbosa.comunic@gmail.com>
2023-12-16 00:42:13 -0300

    Ajuste
    Nesta etapa foi feito o bloqueio do acesso a pagina roteiros,  sendo possível acessar apenas quando logado, foi usado JavaScript para fazer esse block. Foram criadas as duas páginas restantes dos roteiros 2 e 3. Alguns ajustes na barra de menu foram feitos, ainda será necessário fazer alguns ajustes no css de todas as páginas.

--- codigo-fonte/faleConosco.js
+++ codigo-fonte/faleConosco.js
@@ -63,13 +63,56 @@ const formSubmit = new FormSubmit({
   error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
 });
 formSubmit.init();
-
 function clickMenu() {
+  // Recupera a referência para o elemento com o id "itens"
+  var itens = document.getElementById('itens');
+
   if (itens.style.display == 'block') {
-      itens.style.display = 'none'
+    itens.style.display = 'none';
   } else {
-      itens.style.display = 'block'
+    itens.style.display = 'block';
   }
-  
 }
+
+// Função para verificar se o usuário está logado e se tem permissão para acessar uma determinada página
+function isUsuarioAutorizado(paginaAlvo) {
+  // Recupera os dados do localStorage
+  var storedUsuario = localStorage.getItem('usuario');
+
+  // Verifica se o usuário está logado
+  if (storedUsuario !== null) {
+    // Converte a string JSON armazenada de volta para um objeto
+    var usuario = JSON.parse(storedUsuario);
+
+    // Adicione aqui a lógica para verificar se o usuário tem permissão para acessar a página alvo
+    // Neste exemplo, vamos permitir o acesso a todas as páginas se o usuário estiver logado
+    return true;
+  } else {
+    // Se o usuário não está logado, não tem permissão
+    return false;
+  }
+}
+
+// Exemplo de uso
+var paginaAlvo = "roteiros.html"; // Substitua isso pelo nome da página que você deseja verificar
+if (isUsuarioAutorizado(paginaAlvo)) {
+  console.log('O usuário está autorizado a acessar ' + paginaAlvo);
+  // Adicione aqui o código para redirecionar para a página alvo ou qualquer outra ação
+} else {
+  console.log('O usuário não está autorizado a acessar ' + paginaAlvo);
+  // Adicione aqui o código para redirecionar para uma página de login ou outra ação
+}
+
+// Função para ocultar o item de Roteiros se o usuário não estiver logado
+function ocultarRoteirosSeNaoLogado() {
+  var roteirosItem = document.getElementById('roteirosItem');
+  if (isUsuarioLogado()) {
+      roteirosItem.style.display = 'none';
+  } else {
+      roteirosItem.style.display = 'block';
+  }
+}
+
+// Chama a função ao carregar a página
+document.addEventListener('DOMContentLoaded', ocultarRoteirosSeNaoLogado);
 
